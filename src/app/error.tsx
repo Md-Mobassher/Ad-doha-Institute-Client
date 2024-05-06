8ffdba852de16490a653a7c70c8debde55cacf35
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-05-07 05:48:02 +0600

    nextconfig configured

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,8 +1,26 @@
 "use client";
-const ErrorPage = () => {
+
+const ErrorPage = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
   return (
-    <div>
-      <h1>This is error component</h1>
+    <div className="mt-10  text-center">
+      <p className="text-4xl bg-red-500 text-white p-5 w-[50%] mx-auto rounded-xl">
+        Something went wrong!!!
+      </p>
+      <p className="text-4xl bg-red-500 text-white p-5 w-[50%] mx-auto rounded-xl mt-2">
+        {error.message}
+      </p>
+      <button
+        onClick={() => reset()}
+        className="btn btn-error btn-outline mt-5"
+      >
+        Try Again
+      </button>
     </div>
   );
 };
