4c425397ab72ba70074dbe0f3daf7586d1b93c83
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-05-07 12:09:04 +0600

    advisory committe added

---
+++ src/app/(withAboutLayout)/about/advisory-committee/[committeeId]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const AdvisoryCommitteeDetailsPage = ({ params }: any) => {
+  console.log(params);
+  return <div>AdvisoryCommitteeDetailsPage</div>;
+};
+
+export default AdvisoryCommitteeDetailsPage;
--- src/app/(withAboutLayout)/about/advisory-committee/page.tsx
+++ src/app/(withAboutLayout)/about/advisory-committee/page.tsx
@@ -1,5 +1,27 @@
+import { advisoryCommitteData } from "@/data/advisoryCommittee";
+import Image from "next/image";
+
 const AdvisoryCommitteePage = () => {
-  return <div>AdvisoryCommitteePage</div>;
+  return (
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 p-5">
+      {advisoryCommitteData &&
+        advisoryCommitteData.map((committee) => (
+          <div key={committee.id} className="border shadow-lg rounded-lg">
+            <div>
+              <Image
+                src={committee.image}
+                alt={committee.name}
+                className="rounded-t-lg"
+              />
+            </div>
+            <div className="p-4">
+              <h3 className="text-xl font-bold mb-2">{committee.name}</h3>
+              <h5>{committee.designation}</h5>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default AdvisoryCommitteePage;
